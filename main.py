833ecede045edcaf371bd63701ccc8b753adb1a9
ziminson <ziminson@gmail.com>
2023-10-13 06:59:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import serial
-import time
 
 ser = serial.Serial('COM4', 115200, timeout=1)
 
 while 1:
-    ser.write(str('234,123,234').encode())
-    stst = ser.readline().decode()
-    print (stst)
+    ser.write(str('234,123,234').encode()) #передаем строчку в ком-порт, улетает в виде массива байтов
+    stst = ser.readline().decode() #ловим ответочку от платы
+    print (stst) #выводим на экран
 
